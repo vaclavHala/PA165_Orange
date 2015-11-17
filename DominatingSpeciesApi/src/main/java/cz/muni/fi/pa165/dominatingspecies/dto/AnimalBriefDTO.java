713cac6a8fe776650f26655b68d1aa4ce49554c1
Petr <petr.domkar@gmail.com>
2015-11-17 11:53:24 +0100

    Edit AnimalDao, Add BeanMapping, Add dependency, Make basic AnimalService and start AnimalFacadeImpl

--- DominatingSpeciesApi/src/main/java/cz/muni/fi/pa165/dominatingspecies/dto/AnimalBriefDTO.java
+++ DominatingSpeciesApi/src/main/java/cz/muni/fi/pa165/dominatingspecies/dto/AnimalBriefDTO.java
@@ -6,4 +6,20 @@ public class AnimalBriefDTO {
     private String name;
     private String species;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
 }
