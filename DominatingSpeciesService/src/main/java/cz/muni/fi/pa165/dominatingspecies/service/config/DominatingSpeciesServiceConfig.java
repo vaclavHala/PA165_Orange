9502643cafd99460daf0fe5d1d8332daf323be0e
Petr <petr.domkar@gmail.com>
2015-11-18 17:01:23 +0100

    Remove import

--- DominatingSpeciesService/src/main/java/cz/muni/fi/pa165/dominatingspecies/service/config/DominatingSpeciesServiceConfig.java
+++ DominatingSpeciesService/src/main/java/cz/muni/fi/pa165/dominatingspecies/service/config/DominatingSpeciesServiceConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(DominatingSpeciesServiceConfig.class)
 @ComponentScan(basePackages = "cz.muni.fi.pa165.dominatingspecies.service")
 public class DominatingSpeciesServiceConfig {
      
